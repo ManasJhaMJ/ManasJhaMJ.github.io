b35a565c2cec3eec3d1679a183359455a5a925b6
Manas Jha <83183824+ManasJhaMJ@users.noreply.github.com>
2023-06-03 16:42:11 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+    ScrollReveal().reveal('.heading1',{delay : 1000,easing: 'ease-in'});
+    ScrollReveal().reveal('.content1',{delay : 1000,easing: 'ease-in'});
+    ScrollReveal().reveal('.table1',{delay : 1000,easing: 'ease-in'});
+    ScrollReveal().reveal('.scroll',{delay : 1000,easing: 'ease-in'});
+
+    ScrollReveal().reveal('.scroll-one',{delay : 600,easing: 'ease-in'});
+    ScrollReveal().reveal('.scroll-two',{delay : 1000,easing: 'ease-in'});
+    ScrollReveal().reveal('.scroll-three',{delay : 1500,easing: 'ease-in'});
+    ScrollReveal().reveal('.scroll-four',{delay : 2000,easing: 'ease-in'});
