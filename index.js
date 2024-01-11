2ca1ffce8721df5216964702a67193b9687e4144
manasjhamj <jhamanas939@gmail.com>
2024-01-11 23:27:34 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
-    ScrollReveal().reveal('.heading1',{delay : 1000,easing: 'ease-in'});
-    ScrollReveal().reveal('.content1',{delay : 1000,easing: 'ease-in'});
-    ScrollReveal().reveal('.table1',{delay : 1000,easing: 'ease-in'});
-    ScrollReveal().reveal('.scroll',{delay : 1000,easing: 'ease-in'});
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
 
-    ScrollReveal().reveal('.scroll-one',{delay : 600,easing: 'ease-in'});
-    ScrollReveal().reveal('.scroll-two',{delay : 1000,easing: 'ease-in'});
-    ScrollReveal().reveal('.scroll-three',{delay : 1500,easing: 'ease-in'});
-    ScrollReveal().reveal('.scroll-four',{delay : 2000,easing: 'ease-in'});
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+function theme() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+
+VanillaTilt.init(document.querySelectorAll(".glassmorphic-card"), {
+    max: 12,
+    speed: 200,
+    glare: true,
+    "max-glare": 0.5,
+});
+
+VanillaTilt.init(document.querySelectorAll(".glassmorphic-card-mobile"), {
+    max: 20,
+    speed: 200,
+    glare: true,
+    "max-glare": 0.7,
+});
